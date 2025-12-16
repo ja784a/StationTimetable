779bdfb8582538413acda234bb97b5d6308d454f
ja784a <kouheikun0124@gmail.com>
2025-12-16 14:36:08 +0900

    12/16

---
+++ StationTimetable/src/test/java/com/station/StationTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationTimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
