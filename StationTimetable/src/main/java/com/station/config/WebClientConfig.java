98d6bce04a510a484a9d6bc5589e736b42451908
ja784a <kouheikun0124@gmail.com>
2025-12-25 11:42:07 +0900

    12/25

--- StationTimetable/src/main/java/com/station/config/WebClientConfig.java
+++ StationTimetable/src/main/java/com/station/config/WebClientConfig.java
@@ -2,15 +2,22 @@ package com.station.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class WebClientConfig {
 
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-        		.baseUrl("https://api.odpt.org")
-        		.build();
-    }
+	@Bean
+	public WebClient webClient() {
+	    DefaultUriBuilderFactory factory =
+	        new DefaultUriBuilderFactory("https://api.odpt.org");
+	    factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+
+	    return WebClient.builder()
+	        .uriBuilderFactory(factory)
+	        .defaultHeader(HttpHeaders.USER_AGENT, "StationTimetable/1.0")
+	        .build();
+	}
 }
